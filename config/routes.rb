7e848e3a8002a0c4fe66aa8b721e5147b6643020
Péter Nagy <peter@meter-reader.com>
2021-02-05 23:58:03 +0100

    Update nio4r gem

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@
 #
 
 Barion::Engine.routes.draw do
-  # TODO: implement later (inner comment)
+  get 'callback', to: 'main#callback', as: :server_callback
+  get 'success', to: 'main#success', as: :payment_success
+  get 'fail', to: 'main#failed', as: :payment_failed
 end
