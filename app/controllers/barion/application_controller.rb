a272ab464d2ca7004bb76ed34fda0e24e3cc493a
Peter Nagy <peter@meter-reader.com>
2023-02-13 10:10:19 +0100

    Add missing class comment

--- app/controllers/barion/application_controller.rb
+++ app/controllers/barion/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Barion
+  # Main controller for application
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
   end
