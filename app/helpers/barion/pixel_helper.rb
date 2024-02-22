e088ce0ab55048063979fd6a06fe660b152ad9e9
Péter Nagy <peter@antronin.consulting>
2024-02-22 09:15:12 +0100

    Add support for noscript tag and all tags

--- app/helpers/barion/pixel_helper.rb
+++ app/helpers/barion/pixel_helper.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
-# typed: true
-
+# Barion related functions
 module Barion
   # Adds Barion Pixel javascript to views
+  require 'action_view'
+  # Provides functions for Barion Pixel implementations in views
   module PixelHelper
+    include ActionView::Helpers::TagHelper
     def pixel_basic_js
       javascript_tag %(
       window["bp"] = window["bp"] || function () {(window["bp"].q = window["bp"].q || []).push(arguments);};
@@ -18,6 +20,19 @@ module Barion
       // Send init event
       bp('init', 'addBarionPixelId', window['barion_pixel_id']);'
 ).gsub!(/^#{[/\A\s*/]}/, '')
+    end
+
+    def pixel_basic_noscript
+      tag.noscript do
+        tag.img height: 1, width: 1,
+                style: 'display:none',
+                alt: 'Barion Pixel',
+                src: "https://pixel.barion.com/a.gif?ba_pixel_id='#{::Barion.pixel_id}'&ev=contentView&noscript=1"
+      end
+    end
+
+    def pixel_basic_tags
+      pixel_basic_js + pixel_basic_noscript
     end
   end
 end
--- test/helpers/pixel_helper_test.rb
+++ test/helpers/pixel_helper_test.rb
@@ -5,8 +5,21 @@ require 'test_helper'
 module Barion
   class PixelHelperTest < ActionView::TestCase
     include Barion::PixelHelper
+
+    setup do
+      ::Barion.pixel_id = 'BP-1234567890-12'
+    end
+
     test 'JS contains pixel ID' do
       assert_match(/#{::Barion.pixel_id}/, pixel_basic_js)
+    end
+
+    test 'Noscript contains pixel ID' do
+      assert_match(/#{::Barion.pixel_id}/, pixel_basic_noscript)
+    end
+
+    test 'all tags contains pixel ID' do
+      assert_match(/#{::Barion.pixel_id}/, pixel_basic_tags)
     end
   end
 end
