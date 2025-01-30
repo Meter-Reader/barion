2e2a78c79cae2fce4d91985483f4215a437729b8
Péter Nagy <peter@antronin.consulting>
2025-01-30 15:15:09 +0100

    Add method documentation

--- lib/barion.rb
+++ lib/barion.rb
@@ -13,16 +13,33 @@ module Barion
 
   PIXELID_PATTERN = ::Regexp.new('BP-.{10}-\d{2}').freeze
 
+  # Returns the deprecator instance for the Barion module.
+  #
+  # This is a singleton method that initializes the deprecator
+  # if it hasn't been initialized yet, using ActiveSupport::Deprecation.
+  #
+  # @return [ActiveSupport::Deprecation] the deprecator instance
   def self.deprecator
     @deprecator ||= ActiveSupport::Deprecation.new('1.0', 'Barion')
   end
 
+  # Configure the Barion engine
+  #
+  # @example
+  #   ::Barion.config do |conf|
+  #     conf.poskey = 'test'
+  #     conf.sandbox = true
+  #   end
   def self.config(&_block)
     return Barion::Config.instance unless block_given?
 
     yield(Barion::Config.instance)
   end
 
+  # Returns whether the Barion engine is in sandbox mode.
+  #
+  # @return [boolean] whether the engine is in sandbox mode
+  # @deprecated
   def self.sandbox
     config.sandbox
     deprecator.warn(
@@ -31,6 +48,11 @@ module Barion
     )
   end
 
+  # Sets whether the Barion engine is in sandbox mode.
+  #
+  # @param val [boolean] the desired state for sandbox mode
+  #   true to enable sandbox mode, false to disable it
+  # @deprecated Use ::Barion.config.sandbox= instead
   def self.sandbox=(val)
     config.sandbox = val
     deprecator.warn(
@@ -39,6 +61,13 @@ module Barion
     )
   end
 
+  # Returns whether the Barion engine is in sandbox mode.
+  #
+  # This method delegates to the Barion::Config instance's sandbox? method
+  # to determine the current sandbox mode setting.
+  #
+  # @return [Boolean] true if in sandbox mode, false otherwise
+  # @deprecated Use ::Barion.config.sandbox? instead.
   def self.sandbox?
     config.sandbox?
     deprecator.warn(
@@ -47,6 +76,10 @@ module Barion
     )
   end
 
+  # Returns the current POSKey setting.
+  #
+  # @return [String, nil] the current POSKey setting
+  # @deprecated Use ::Barion.config.poskey instead.
   def self.poskey
     config.poskey
     deprecator.warn(
@@ -55,6 +88,10 @@ module Barion
     )
   end
 
+  # Sets the POSKey for the shop.
+  #
+  # @param val [String] the POSKey provided by Barion
+  # @deprecated Use ::Barion.config.poskey= instead
   def self.poskey=(val)
     config.poskey = (val)
     deprecator.warn(
@@ -63,6 +100,9 @@ module Barion
     )
   end
 
+  # Returns the current PublicKey setting.
+  #
+  # @return [String, nil] the current PublicKey setting
   def self.publickey
     config.publickey
     deprecator.warn(
@@ -71,6 +111,9 @@ module Barion
     )
   end
 
+  # Sets the PublicKey for the shop.
+  #
+  # @param val [String] the PublicKey provided by Barion
   def self.publickey=(val)
     config.publickey = (val)
     deprecator.warn(
@@ -79,6 +122,9 @@ module Barion
     )
   end
 
+  # Returns the current acronym for Barion.
+  #
+  # @return [String] the current acronym setting, empty string by default
   def self.acronym
     config.acronym
     deprecator.warn(
@@ -87,6 +133,9 @@ module Barion
     )
   end
 
+  # Sets the acronym for Barion.
+  #
+  # @param val [String] the desired acronym, empty string by default
   def self.acronym=(val)
     config.acronym = val
     deprecator.warn(
@@ -95,6 +144,12 @@ module Barion
     )
   end
 
+  # Returns the current Barion Pixel ID.
+  #
+  # The Pixel ID is used for tracking and analytics purposes.
+  #
+  # @return [String, nil] the current Pixel ID setting
+  # @deprecated Use ::Barion.config.pixel_id instead.
   def self.pixel_id
     config.pixel_id
     deprecator.warn(
@@ -103,6 +158,13 @@ module Barion
     )
   end
 
+  # Sets the Barion Pixel ID.
+  #
+  # Updates the Barion configuration with the provided Pixel ID.
+  # This method is deprecated and it is recommended to use
+  # `::Barion.config.pixel_id=` instead.
+  #
+  # @param val [String] the Pixel ID to set
   def self.pixel_id=(val)
     config.pixel_id = val
     deprecator.warn(
@@ -111,6 +173,13 @@ module Barion
     )
   end
 
+  # Returns the current default payee for Barion.
+  #
+  # The default payee is the default email address to which the payment
+  # confirmation email is sent.
+  #
+  # @return [String] the current default payee setting, empty string by default
+  # @deprecated Use ::Barion.config.default_payee instead
   def self.default_payee
     config.default_payee
     deprecator.warn(
@@ -119,6 +188,14 @@ module Barion
     )
   end
 
+  # Sets the default payee for Barion.
+  #
+  # Updates the Barion configuration with the provided default payee.
+  # This method is deprecated and it is recommended to use
+  # `::Barion.config.default_payee=` instead.
+  #
+  # @param val [String] the default payee to set
+  # @deprecated Use ::Barion.config.default_payee= instead
   def self.default_payee=(val)
     config.default_payee = val
     deprecator.warn(
@@ -127,6 +204,11 @@ module Barion
     )
   end
 
+  # Returns the class used for user authentication.
+  #
+  # This method is deprecated and it is recommended to use
+  # `::Barion.config.user_class` instead.
+  # @deprecated Use ::Barion.config.user_class instead
   def self.user_class
     config.user_class
     deprecator.warn(
@@ -135,6 +217,14 @@ module Barion
     )
   end
 
+  # Sets the class used for user authentication.
+  #
+  # Updates the Barion configuration with the provided user class.
+  # This method is deprecated and it is recommended to use
+  # `::Barion.config.user_class=` instead.
+  #
+  # @param val [Class] the class to use for user authentication
+  # @deprecated Use ::Barion.config.user_class= instead
   def self.user_class=(val)
     config.user_class = val
     deprecator.warn(
@@ -143,6 +233,13 @@ module Barion
     )
   end
 
+  # Returns the class used for item representation.
+  #
+  # This method is deprecated and it is recommended to use
+  # `::Barion.config.item_class` instead.
+  # @deprecated Use ::Barion.config.item_class instead
+  # @return [Class] the class used for item representation
+  # @deprecated Use ::Barion.config.item_class instead
   def self.item_class
     config.item_class
     deprecator.warn(
@@ -151,6 +248,15 @@ module Barion
     )
   end
 
+  # Sets the class used for item representation.
+  #
+  # Updates the Barion configuration with the provided item class.
+  # This method is deprecated and it is recommended to use
+  # `::Barion.config.item_class=` instead.
+  #
+  # @param val [Class] the class to use for item representation
+  # @deprecated Use ::Barion.config.item_class= instead
+  # @deprecated Use ::Barion.config.item_class= instead
   def self.item_class=(val)
     config.item_class = val
     deprecator.warn(
@@ -159,6 +265,12 @@ module Barion
     )
   end
 
+  # Returns the class used for making REST calls to the Barion API.
+  #
+  # This method is deprecated and it is recommended to use
+  # `::Barion.config.rest_client_class` instead.
+  # @deprecated Use ::Barion.config.rest_client_class instead
+  # @return [Class] the class used for making REST calls
   def self.rest_client_class
     config.rest_client_class
     deprecator.warn(
@@ -167,6 +279,14 @@ module Barion
     )
   end
 
+  # Sets the class used for making REST calls to the Barion API.
+  #
+  # Updates the Barion configuration with the provided REST client class.
+  # This method is deprecated and it is recommended to use
+  # `::Barion.config.rest_client_class=` instead.
+  #
+  # @param val [Class] the class to use for making REST calls
+  # @deprecated Use ::Barion.config.rest_client_class= instead
   def self.rest_client_class=(val)
     config.rest_client_class = val
     deprecator.warn(
@@ -181,8 +301,18 @@ module Barion
 
   # Generic error class for Barion module
   class Error < StandardError
-    attr_reader :title, :error_code, :happened_at, :auth_data, :endpoint, :errors
+    attr_reader :title, :error_code, :happened_at, :auth_data, :endpoint, :errors, :description
 
+    # Creates a new instance of the Barion::Error class. It can hold nested errors.
+    #
+    # @param params [Hash] a hash containing the error data
+    # @option params [String] :Title the title of the error
+    # @option params [Integer] :ErrorCode the error code
+    # @option params [String] :HappenedAt when the error happened in ISO 8601 format
+    # @option params [String] :AuthData authentication data
+    # @option params [String] :Endpoint the endpoint where the error happened
+    # @option params [Array<Hash>] :Errors the errors that happened
+    # @option params [String] :Description the description of the error
     def initialize(params)
       @title = params[:Title]
       @error_code = params[:ErrorCode]
@@ -190,9 +320,13 @@ module Barion
       @auth_data = params[:AuthData]
       @endpoint = params[:Endpoint]
       @errors = Array(params[:Errors]).map { |e| Barion::Error.new(e) } if params.key? :Errors
-      super(params[:Description])
+      @description = params[:Description]
+      super(@description)
     end
 
+    # Returns all errors that happened in the request in a single string
+    #
+    # @return [String] all errors that happened in the request
     def all_errors
       Array(@errors).map(&:message).join("\n")
     end
--- lib/barion/config.rb
+++ lib/barion/config.rb
@@ -7,18 +7,40 @@ module Barion
   class Config
     include Singleton
 
-    attr_accessor :publickey, :default_payee
+    # @!attribute [rw] publickey
+    #   Set this to the string provided by Barion registration
+    #   @return [String, nil] the public key provided by Barion (default: nil)
+    attr_accessor :publickey
+    # @!attribute [rw] default_payee
+    #   Set this to the registered email address of your shop
+    #   @return [String, nil] registered email address for your Barion shop (default: nil)
+    attr_accessor :default_payee
+    # @!attribute [w] acronym
+    #   Set this to the string provided by Barion registration
+    #   @return [String, nil] the acronym provided by Barion (default: '')
     attr_writer :acronym
+    # @!attribute [r] pixel_id
+    #   The pixel ID set up in the initalizer
+    #   @return [String, nil] the acronym provided by Barion (default: '')
     attr_reader :pixel_id
 
+    # @!attribute [r] poskey
+    #   The POSKey set up in the initalizer
+    #   @return [String, nil] the POSKey provided by Barion (default: nil)
     def poskey
       @_poskey || nil
     end
 
+    # The acronym set up in the initalizer
+    # @return [String] the acronym provided by Barion (default: '')
     def acronym
       @acronym || ''
     end
 
+    # Set the POSKey for the shop
+    #
+    # @param key [String] the POSKey provided by Barion
+    # @raise [ArgumentError] if the key is not a String
     def poskey=(key)
       unless key.is_a?(String)
         raise ArgumentError,
@@ -28,24 +50,45 @@ module Barion
       @_poskey = key
     end
 
+    # Returns the current sandbox mode setting.
+    # Delegates to the sandbox? method to determine the current state.
+    #
+    # @return [Boolean] true if in sandbox mode, false otherwise
     def sandbox
       sandbox?
     end
 
+    # Returns the current sandbox mode setting.
+    #
+    # If the sandbox mode was not set explicitly, it defaults to true.
+    #
+    # @return [Boolean] true if in sandbox mode, false otherwise
     def sandbox?
       @_sandbox = true if @_sandbox.nil?
       @_sandbox
     end
 
+    # Sets the sandbox mode.
+    #
+    # @param val [Boolean] the desired state for sandbox mode
+    #   true to enable sandbox mode, false to disable it
     def sandbox=(val)
       @_sandbox = !!val
     end
 
+    # Sets the Barion Pixel ID.
+    #
+    # Validates that the provided ID is a string and matches the Barion Pixel ID format.
+    #
+    # @param id [String] the Barion Pixel ID to set
+    # @raise [ArgumentError] if the ID is not a string or does not match the expected format
     def pixel_id=(id)
       unless id.is_a?(String)
         raise ArgumentError,
               "::Barion::Config.pixel_id must be set to a ::String, got #{id.inspect}"
       end
+
+      return if id.empty?
 
       if PIXELID_PATTERN.match(id).nil?
         raise ::ArgumentError,
@@ -55,11 +98,25 @@ module Barion
       @pixel_id = id
     end
 
+    # Returns the REST client endpoint based on the current environment.
+    #
+    # Determines the environment by checking if the sandbox mode is enabled.
+    # If sandbox mode is enabled, the test environment URL is used; otherwise,
+    # the production environment URL is used.
+    #
+    # @return [Object] a new instance of the REST client class initialized with
+    #   the appropriate base URL for the current environment.
     def endpoint
       env = sandbox? ? :test : :prod
       rest_client_class.new ::Barion::BASE_URL[env]
     end
 
+    # Sets the user class used for user authentication.
+    #
+    # Validates that the provided user class is a string.
+    #
+    # @param class_name [String] the user class to use for user authentication
+    # @raise [ArgumentError] if the user class is not a string
     def user_class=(class_name)
       unless class_name.is_a?(String)
         raise ArgumentError, "::Barion:.config.user_class must be set to a ::String, got #{class_name.inspect}"
@@ -68,6 +125,9 @@ module Barion
       @_user_class = class_name
     end
 
+    # Returns the user class set in the initializer.
+    #
+    # @return [Class] the class set in the initializer
     def user_class
       # This is nil before the initializer is installed.
       return nil if @_user_class.nil?
@@ -75,6 +135,9 @@ module Barion
       @_user_class.constantize
     end
 
+    # Sets the class used for item representation.
+    #
+    # @param val [Class] the class to use for item representation
     def item_class=(class_name)
       unless class_name.is_a?(String)
         raise ArgumentError, "::Barion.config.item_class must be set to a ::String, got #{class_name.inspect}"
@@ -83,6 +146,11 @@ module Barion
       @_item_class = class_name
     end
 
+    # Returns the item class set in the initializer.
+    #
+    # This method returns +nil+ until the initializer is installed.
+    #
+    # @return [Class] the class set in the initializer
     def item_class
       # This is nil before the initializer is installed.
       return nil if @_item_class.nil?
@@ -90,10 +158,22 @@ module Barion
       @_item_class.constantize
     end
 
+    # Returns the class used for making REST calls to the Barion API.
+    #
+    # If it was not set explicitly, it defaults to `::RestClient::Resource`.
+    #
+    # @return [Class] the class used for making REST calls
     def rest_client_class
       (@_rest_client_class || '::RestClient::Resource').constantize
     end
 
+    # Sets the class used for making REST calls to the Barion API.
+    #
+    # The class set with this method is used for making REST calls to the Barion API.
+    # It should be set to a class that responds to the same methods as
+    # `::RestClient::Resource`.
+    #
+    # @param class_name [Class, String] the class to use for making REST calls
     def rest_client_class=(class_name)
       unless class_name.is_a?(String)
         raise ArgumentError, "::Barion::Config.rest_client_class must be set to a ::String, got #{class_name.inspect}"
